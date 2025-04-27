9c523e662616f6eacc9a7efa8aa62da440530dd5
loretta-dash <lorettaquansah04@gmail.com>
2025-04-27 04:14:03 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '91702395151X', //Set your number here without + symbol, just add country code & number without any space
+  botName: "BCTH bot",
+  botOwner: 'McRans', // Your name
+  ownerNumber: '233593064505', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
